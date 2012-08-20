ffba65cf6f06c08f9bd3dc886eb7109cbd769eb8
derrekc <dlc32@duke.edu>
2012-08-20 15:55:18 -0400

    added template file for digital collections discovery panel

---
+++ discovery/discovery-digicoll.tpl.php
@@ -0,0 +1,17 @@
+<div class="moreResources" style="clear: both; float: left; ">
+	<p style="float: left; margin: 0; color: #888;"><strong><a title="Click here to search for digitized photos, ads, documents, sheet music and more from the Duke Digital Collections program." onclick="pageTracker._trackEvent('homepageSearchBox', 'dcHomeLink');" href="http://library.duke.edu/digitalcollections">Search Digital Collections >></a></strong><br />Find digitized historic photos, ads, documents, &amp; more</p>
+
+<div class="digCollImages">
+	<a onclick="pageTracker._trackEvent('homepageSearchBox', 'dcExample', 'eaa.D0369');" href="http://library.duke.edu/digitalcollections/eaa_D0369/"><img src="http://library.duke.edu/imgs/homepage/digcoll-images/thumbdurham.jpg" alt="Emergence of Advertising in America" title="Durham Whiffs (Collection: Emergence of Advertising in America)" /></a>
+	<a  onclick="pageTracker._trackEvent('homepageSearchBox', 'dcExample', 'hasm.b2030');" href="http://library.duke.edu/digitalcollections/hasm_b2030/"><img src="http://library.duke.edu/imgs/homepage/digcoll-images/thumblincoln.jpg" alt="Historic American Sheet Music" title="Lincoln's Funeral March (Collection: Historic American Sheet Music)" /></a>
+	<a  onclick="pageTracker._trackEvent('homepageSearchBox', 'dcExample', 'hasm.a5378');" href="http://library.duke.edu/digitalcollections/hasm_a5378/"><img src="http://library.duke.edu/imgs/homepage/digcoll-images/thumbmusic.jpg" alt="Historic American Sheet Music" title="Alexander's Ragtime Band (Collection: Historic American Sheet Music)" /></a>
+	<a  onclick="pageTracker._trackEvent('homepageSearchBox', 'dcExample', 'duc.ducph02003');" href="http://library.duke.edu/digitalcollections/duc_ducph02003/"><img src="http://library.duke.edu/imgs/homepage/digcoll-images/thumbchapel.jpg" alt="Construction of Duke University" title="Chapel and School of Religion, Duke University (Collection: Construction of Duke University)" /></a>
+	<a  onclick="pageTracker._trackEvent('homepageSearchBox', 'dcExample', 'adaccess.bh1939');" href="http://library.duke.edu/digitalcollections/adaccess_BH1939/"><img src="http://library.duke.edu/imgs/homepage/digcoll-images/thumbadaccess.jpg" alt="Ad*Access" title="Marilyn Monroe Discovers The World's Most Glamorous Make-up...From The Westmores of Hollywood (Collection: Ad*Access)" /></a>
+	<a  onclick="pageTracker._trackEvent('homepageSearchBox', 'dcExample', 'gamble.512-2962');" href="http://library.duke.edu/digitalcollections/gamble_512-2962/"><img src="http://library.duke.edu/imgs/homepage/digcoll-images/thumbgamble.jpg" alt="Sidney D. Gamble Photographs" title="Woman &amp; Baby (Collection: Sidney D. Gamble Photographs)" /></a>
+	<a  onclick="pageTracker._trackEvent('homepageSearchBox', 'dcExample', 'dsp.dspfb15395/pg.1/');" href="http://library.duke.edu/digitalcollections/dsp_dspfb15395/"><img src="http://library.duke.edu/imgs/homepage/digcoll-images/thumbfootball.jpg" alt="Duke Football Programs" title="Duke vs. University of North Carolina, 1981 Nov 21 (Collection: Duke Football Programs)" /></a>
+
+	</div> <!-- /digCollImages -->
+
+<div style="clear: both;"></div>
+
+</div> <!-- /moreResources -->
