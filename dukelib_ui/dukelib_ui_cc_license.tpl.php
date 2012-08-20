fd91b57cf752c345046c57b4914b4b2d869bde73
derrekc <dlc32@duke.edu>
2012-08-20 11:07:22 -0400

    new module that provides blocks for 'social_links' and 'creative commons/share' content'

---
+++ dukelib_ui/dukelib_ui_cc_license.tpl.php
@@ -0,0 +1,10 @@
+<?php
+
+?>
+<div id="shareThis1"> 
+  <span class="st_sharethis" displayText="ShareThis"></span> 
+</div> 		
+<div id="ccCopyright"><!-- Start CC footer --> 
+<a href="http://creativecommons.org/licenses/by-nc-sa/3.0/us/" rel="license"><img alt="Creative Commons License" src="http://i.creativecommons.org/l/by-nc-sa/3.0/80x15.png" style="border-width:0" width="80" height="15" /></a> 
+<p style="margin-top: 20px;">Unless otherwise specified on this page, this work is licensed under a<br /><a href="http://creativecommons.org/licenses/by-nc-sa/3.0/us/" rel="license">Creative Commons Attribution-Noncommercial-Share Alike 3.0 United States License</a>.</p> 
+</div>
---
+++ dukelib_ui/dukelib_ui_social_links.tpl.php
@@ -0,0 +1,13 @@
+<?php
+
+?>
+<p>
+	<a href="http://library.duke.edu/connect/index.php" title="follow us on Twitter"><img src="http://library.duke.edu/imgs/blue-note/twitter30x30.png" alt="follow us on Twitter" /></a>&nbsp;
+	<a href="http://library.duke.edu/connect/facebook.html" title="follow us on Facebook"><img src="http://library.duke.edu/imgs/blue-note/facebook30x30.png" alt="follow us on Facebook" /></a>&nbsp;
+	<a href="http://library.duke.edu/connect/youtube.html" title="follow us on YouTube"><img src="http://library.duke.edu/imgs/blue-note/youtube30x30.png" alt="follow us on YouTube" /></a>&nbsp;
+	<a href="http://library.duke.edu/connect/flickr.html" title="follow us on Flickr"><img src="http://library.duke.edu/imgs/blue-note/flickr30x30.png" alt="follow us on Flickr" /></a>&nbsp;
+	<a href="http://library.duke.edu/blogs/" title="follow our blogs and feeds"><img src="http://library.duke.edu/imgs/blue-note/rss30x30.png" alt="follow our blogs and feeds" /></a>
+</p>
+<p id="circdesk">
+	<a href="http://library.duke.edu/about/contact.html" title="Contact Us">919-660-5870<br />(Perkins Circulation Desk)</a>
+</p>
